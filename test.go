74ec7081c0f687c9d5c8dfccf323d95cd89317b0
PietaTony <sjmtony@gmail.com>
2019-06-13 14:07:49 +0800

    Update README.md

--- test.go
+++ test.go
@@ -1,8 +1,18 @@
 package main
 
-import . "github.com/PietaTony/APILib/httpServer"
+import . "github.com/PietaTony/APILib/SMTP"
 
 func main(){
-	HttpServer("8080")
-	fmt("end test")
+	fromMail := "sjmtony@gmail.com"
+	toMail := "sjmtony@gmail.com"
+    subj := "This is the email subject"
+    body := "This is an example body.\n With two lines."
+    SMTPName := "smtp.gmail.com:465"
+    SMTPMail := "sjmtony@gmail.com"
+    SMTPPassword := "Sjm778887"
+
+	SMTPSend( fromMail, toMail,
+			  subj, body,
+			  SMTPName,
+			  SMTPMail, SMTPPassword )
 }
