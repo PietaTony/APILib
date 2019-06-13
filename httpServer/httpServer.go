f75e3e4a1ab98b1921459ceb1ed0a4a3f6aac7b8
PietaTony <sjmtony@gmail.com>
2019-06-13 11:33:56 +0800

    Add files via upload

--- httpServer/httpServer.go
+++ httpServer/httpServer.go
@@ -1,11 +1,12 @@
 package httpServer
 
 import (
+    "strconv"
 	"fmt"
 	"net/http"
 )
 
-func HttpServer() {
+func HttpServer(port string) {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")
 	})
@@ -13,5 +14,7 @@ func HttpServer() {
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":" + port, nil)
+
+	fmt.Println("http server is running at http://localhost:" + port)
 }
